56e49c573a1fed130051294723843444e8a96a00
Teodora Nesheva <105648772+TeodoraNesheva@users.noreply.github.com>
2023-05-10 10:53:29 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/Number100...200.js
@@ -0,0 +1,11 @@
+function number100(input) {
+    let userInput = input[0];
+ 
+    if (userInput < 100) {
+        console.log("Less than 100");
+    } else if (userInput <= 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+    }
+}
---
+++ Conditional Statements - Lab/PasswordGuess.js
@@ -0,0 +1,10 @@
+function passwordGuess(input) {
+    let passwordTexts = input[0];
+  
+    if (passwordTexts == "s3cr3t!P@ssw0rd") {
+      console.log("Welcome");
+    } else {
+      console.log("Wrong password!");
+    }
+  }
+  
