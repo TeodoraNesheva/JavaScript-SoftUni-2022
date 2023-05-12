38a033e1a77d88269d82aea84d4517ac4e6f631e
Teodora Nesheva <105648772+TeodoraNesheva@users.noreply.github.com>
2023-05-12 10:30:34 +0300

    Add files via upload

---
+++ For Loop - Exercise/NumbersEndingIn7.js
@@ -0,0 +1,9 @@
+function solve(input){
+    let i =Number(input[0]);
+
+    for(let i = 7; i <= 997; i++) {
+        if(i % 10 === 7) {
+            console.log(i);
+        }
+    }
+}
---
+++ For Loop - Exercise/Salary.js
@@ -0,0 +1,34 @@
+function salary(input) {
+ 
+    let nOpenTabs = Number(input.shift()) ;
+    let salary = Number(input.shift());
+ 
+for (let i = 0; i < nOpenTabs; i++ ) {
+ 
+       let tab = String(input[i]);
+ 
+        if ( tab == `Facebook` ) {
+ 
+            salary -= 150
+        }
+        if ( tab == `Instagram` ) {
+ 
+            salary -= 100
+ 
+        }
+        if ( tab == `Reddit` ) {
+ 
+            salary -= 50
+ 
+        }
+ 
+        if ( salary <= 0 ) {
+ 
+            console.log(`You have lost your salary.`)
+            break;    
+          }      
+       }  
+      if (salary != 0 ) {
+        console.log(salary)
+      } 
+    }
